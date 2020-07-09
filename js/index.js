9cdd1a297c11cd7612868267fb405474df34971d
Eugene Asare <eugenemindset@gmail.com>
2020-07-09 00:12:25 -0400

    made changes to make transitions much more readable using async/awaits

--- js/index.js
+++ js/index.js
@@ -2,11 +2,26 @@
 var isChanging = false; // dont want to process other changes if one is already happening
 var currentSection = 'home';
 var sections = {
-  'home': null,
-  'edu': null,
-  'exp': null,
-  'skll': null,
-  'proj': null
+  'home': {
+    backgroundColor: '#000000',
+    textColor: '#FFFFFF'
+  },
+  'edu': {
+    backgroundColor: '#00000F',
+    textColor: '#FFFFF0'
+  },
+  'exp': {
+    backgroundColor: '#0000F0',
+    textColor: '#FFFF0F'
+  },
+  'skll': {
+    backgroundColor: '#000F00',
+    textColor: '#FFF0FF'
+  },
+  'proj': {
+    backgroundColor: '#00F000',
+    textColor: '#FF0FFF'
+  },
 }
 
 /*
@@ -16,29 +31,20 @@ var sections = {
 
 function transition(section) {
 
-  if (isChanging) {
-    return;
-  }
-
-
-  if (section != 'home') {
-    if (currentSection != 'home') {
-      hideSection(currentSection);
-    }
-
-    if (section == currentSection) {
-      currentSection = 'home';
+  // ...
+  if (currentSection != 'home' && section != 'home') {
+    if (currentSection == section) {
+      transitionSections(currentSection, 'home');
     } else {
-      showSection(section);
-      currentSection = section;
+      transitionSections(currentSection, section);
     }
+  } else if (currentSection == 'home' && section != 'home') {
+    transitionSections('home', section);
+  } else if (currentSection != 'home' && section == 'home') {
+    transitionSections(currentSection, 'home')
   } else {
-    if (currentSection != 'home') {
-      hideSection(currentSection);
-      currentSection = 'home';
-    }
   }
-
+}
 
   /*
   console.log(isChanging, "section");
@@ -46,7 +52,7 @@ function transition(section) {
     return;
   }
 
-  isChanging = true;
+  isChanging = true;2
 
   var selector = '#' + section;
   var select = $(selector);
@@ -71,15 +77,52 @@ function transition(section) {
     select.animate({opacity: 0}, speed=500, callback=call);
   }
   */
+
+
+async function transitionSections(startSection, endSection) {
+  
+
+  // ignore other transitions while one is occuring
+  if (isChanging) {
+    return;
+  }
+
+  isChanging = true;
+
+  let startSelector = '#' + startSection;
+  let endSelector = '#' + endSection;
+
+  // need to make a custom function to handle color animations
+  if (endSection == "home") {
+    $('body').css('--background-color', '#F1FBFD');
+    $('body').css('--text-color', '#25253D');
+  } else {
+    $('body').css('--background-color', '#25253D');
+    $('body').css('--text-color', '#F1FBFD');
+  }
+
+  // we wait on things to visible disappear
+  // no waiting on first toggle to not have the page resize twice
+  await $(startSelector).animate({opacity: 0}, speed=500).promise();
+  $(startSelector).toggle(speed=500).promise();
+  await $(endSelector).toggle(speed=500).promise();
+  await $(endSelector).animate({opacity: 1}, speed=500).promise();
+
+
+  currentSection = endSection;
+  isChanging = false;
 }
 
+/*
 function hideSection(section) {
   console.log(`closing ${section}...`);
+  $('body').css('--background-color', '#FFFFFF');
 }
 
 function showSection(section) {
   console.log(`opening ${section}...`);
-}
+  $('body').css('--background-color', '#000000');
+}*/
 
 
 
